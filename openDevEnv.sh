3d54f703cac88d0de64b27d62b87632c23372807
Jonas Myrenås <jonas.myrenas@mindler.se>
2021-04-24 17:12:21 +0200

    improvements

--- openDevEnv.sh
+++ openDevEnv.sh
@@ -1,11 +1,11 @@
 osascript -e 'tell application "Terminal" to activate' \
   -e 'tell application "System Events" to keystroke "t" using {command down}' \
-  -e 'tell application "Terminal" to do script "cd ~/Projects/Private/dragonlist/functions && npm run build:watch" in front window'
+  -e 'tell application "Terminal" to do script "cd '$(pwd)'/functions && npm run build:watch" in front window'
 
 osascript -e 'tell application "Terminal" to activate' \
   -e 'tell application "System Events" to keystroke "t" using {command down}' \
-  -e 'tell application "Terminal" to do script "cd ~/Projects/Private/dragonlist/functions && npm run serve:inspect" in front window'
+  -e 'tell application "Terminal" to do script "cd '$(pwd)'/functions && npm run serve:inspect" in front window'
 
 osascript -e 'tell application "Terminal" to activate' \
   -e 'tell application "System Events" to keystroke "t" using {command down}' \
-  -e 'tell application "Terminal" to do script "cd ~/Projects/Private/dragonlist/view && npm run start" in front window'
+  -e 'tell application "Terminal" to do script "cd '$(pwd)'/view && npm run start" in front window'
